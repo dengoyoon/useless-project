8ff1923464d34b79e925f942a06bc737c1cb8ad1
dengoyoon <duhyeoon@gmail.com>
2024-05-04 18:20:54 +0900

    useTimer일단 만듬 보완 필요함

--- apps/life-is-timing/src/App.tsx
+++ apps/life-is-timing/src/App.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
+const useTimer = () => {
+  const [time, setTime] = useState(0);
+  const requestRef = useRef(0);
+  const animate = (t: DOMHighResTimeStamp) => {
+    setTime(t);
+    console.log(t);
+    requestRef.current = requestAnimationFrame(animate);
+  };
+  const start = () => requestAnimationFrame(animate);
+  const stop = () => cancelAnimationFrame(requestRef.current);
+  const reset = () => {
+    setTime(0);
+  };
+  useEffect(() => {
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []);
+
+  return { time, start, stop, reset };
+};
+
 function App() {
-  return <>Hello world</>;
+  const { time, start, stop } = useTimer();
+  return (
+    <>
+      <div>{time}</div>
+      <div>
+        <button onClick={() => start()}>Start</button>
+        <button onClick={() => stop()}>Stop</button>
+        <button>Reset</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
