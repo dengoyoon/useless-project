db580d1084bb56b0230b2b8ca248f638bee660b8
dengoyoon <duhyeoon@gmail.com>
2024-05-18 16:22:26 +0900

    start, stop, reset 구현

--- apps/life-is-timing/src/App.tsx
+++ apps/life-is-timing/src/App.tsx
@@ -2,47 +2,44 @@ import { useEffect, useRef, useState } from "react";
 
 const useTimer = () => {
   const [time, setTime] = useState(0);
+  const [timerState, setTimerState] = useState("INIT");
   const requestId = useRef(0);
+
   const animate = () => {
-    setTime((prev) => prev + 1);
+    setTime((prevTime) => prevTime + 1);
     requestId.current = requestAnimationFrame(animate);
   };
   const start = () => {
+    setTimerState("RUN");
     requestId.current = requestAnimationFrame(animate);
   };
   const stop = () => {
+    setTimerState("STOP");
     cancelAnimationFrame(requestId.current);
   };
   const reset = () => {
+    setTimerState("INIT");
     setTime(0);
   };
   useEffect(() => {
     return () => cancelAnimationFrame(requestId.current);
   }, []);
 
-  return { time, start, stop, reset };
-};
-
-const useToggle = (initState: boolean = false) => {
-  const [on, setOn] = useState(initState);
-  const toggle = () => setOn((prev) => !prev);
-  return { on, toggle };
+  return { time, timerState, start, stop, reset };
 };
 
 function App() {
-  const { time, start, stop, reset } = useTimer();
-  const { on, toggle } = useToggle();
+  const { time, timerState, start, stop, reset } = useTimer();
   return (
     <>
       <div>{time}</div>
-      <div onClick={() => toggle()}>
-        {on ? (
-          <div>
-            <button onClick={() => stop()}>Stop</button>
-            <button onClick={() => reset()}>Reset</button>
-          </div>
-        ) : (
+      <div>
+        {timerState === "INIT" && (
           <button onClick={() => start()}>Start</button>
+        )}
+        {timerState === "RUN" && <button onClick={() => stop()}>Stop</button>}
+        {timerState === "STOP" && (
+          <button onClick={() => reset()}>Reset</button>
         )}
       </div>
     </>
