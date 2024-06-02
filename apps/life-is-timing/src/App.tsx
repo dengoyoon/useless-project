f5f36d1ebd50b4446e7b4fc1e6236417f70a8d60
DuHyeon Yoon <duhyeoon@gmail.com>
2024-06-02 18:16:31 +0900

    게임: 인생은 타이밍 개발 완료 (#1)
    * useTimer일단 만듬 보완 필요함
    
    * useTimer 훅 완성
    
    * useToggle 훅 구현
    
    * start, stop, reset 구현
    
    * 타이머 구현
    
    * 약간의 오차 있지만 게임 구현 완료
    
    * spacebar 클릭으로 실행 구현
    
    * 각 모듈 분리시키고 절대 경로 적용

--- apps/life-is-timing/src/App.tsx
+++ apps/life-is-timing/src/App.tsx
@@ -1,5 +1,81 @@
+import { useGame, useTimer, useSpaceBarClick } from "hooks";
+
 function App() {
-  return <>Hello world</>;
+  const { time, timerState, clickTimer } = useTimer();
+  const { level, goalTime, setLevel, displayTime, calculateMistake } =
+    useGame();
+
+  const mistakeTime = calculateMistake(time);
+  const failMessage = [
+    "당신은 실패자 입니다.",
+    "당신은 사람이 맞습니까?",
+    "당신은 재능이 없습니다.",
+    "당신은 답이 없습니다.",
+  ];
+
+  useSpaceBarClick(() => {
+    clickTimer.next().value();
+  });
+
+  return (
+    <div>
+      <div>난이도 선택 : {level}</div>
+      <div>
+        <button
+          disabled={timerState !== "INIT"}
+          onClick={() => setLevel("EASY")}
+        >
+          EASY
+        </button>
+        <button
+          disabled={timerState !== "INIT"}
+          onClick={() => setLevel("NORMAL")}
+        >
+          NORMAL
+        </button>
+        <button
+          disabled={timerState !== "INIT"}
+          onClick={() => setLevel("HARD")}
+        >
+          HARD
+        </button>
+      </div>
+      <div>당신의 목표는 {goalTime}초 입니다</div>
+      <div>{displayTime(time)}</div>
+      <button
+        onKeyDown={(event) => {
+          event.stopPropagation();
+        }}
+        onClick={() => {
+          clickTimer.next().value();
+        }}
+      >
+        {
+          {
+            INIT: "시작",
+            RUN: "지금이야!",
+            STOP: "다시?",
+          }[timerState]
+        }
+      </button>
+      {timerState === "STOP" && (
+        <div>
+          {mistakeTime === 0 && <div>최고에요 ^^</div>}
+          {mistakeTime < 0 && (
+            <div>당신은 {displayTime(Math.abs(mistakeTime))}초 느렸습니다.</div>
+          )}
+          {mistakeTime > 0 && (
+            <div>당신은 {displayTime(Math.abs(mistakeTime))}초 빨랐습니다.</div>
+          )}
+          {mistakeTime !== 0 && (
+            <div>
+              {failMessage[Math.floor(Math.random() * failMessage.length)]}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ apps/life-is-timing/src/type.ts
@@ -0,0 +1,2 @@
+export type Level = "EASY" | "NORMAL" | "HARD";
+export type TimerState = "INIT" | "RUN" | "STOP";
